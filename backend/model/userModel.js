6f2c5a702ffe79be6ca2b3d2db92ab78648a02f0
lolko7ol <Mohammed_20225218@fci.helwan.edu.eg>
2024-12-03 04:34:59 +0200

    not lol

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import validator from 'validator';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'; // Note: Corrected 'jwt' to 'jsonwebtoken'
-//lol
+
 const userSchema = new mongoose.Schema({
     userId: {
         type: Number,
