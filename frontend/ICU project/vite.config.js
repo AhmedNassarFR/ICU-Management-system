0c4840bf087aacaa2e08ec0223259be382a2945e
lolko7ol <Mohammed_20225218@fci.helwan.edu.eg>
2024-12-10 04:54:37 +0200

    FrontPrototype

--- frontend/ICU project/vite.config.js
+++ frontend/ICU project/vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslintConfig from "./eslint.config";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintConfig()],
+  plugins: [react()],
 });
