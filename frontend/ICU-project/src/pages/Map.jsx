b62d7cc87f4e11a75c7ccf914f5923827f295a27
Mohamad Reda <dragoongames24@gmail.com>
2024-12-13 09:36:26 +0200

    edit to the map css

--- frontend/ICU-project/src/pages/Map.jsx
+++ frontend/ICU-project/src/pages/Map.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import styles from "./Map.module.css";
+import Icus from "./Icus.jsx";
 
 function Map() {
   // State to store latitude and longitude values
@@ -36,13 +38,12 @@ function Map() {
   }
 
   return (
-    <div className="App">
-      <h1>Your Location on the Map</h1>
-      <MapContainer
-        center={[lat, lng]}
-        zoom={18}
-        style={{ width: "50vh", height: "50vh" }}
-      >
+    <div className={styles.container}>
+      <div>
+        <Icus />
+      </div>
+      <h1 className={styles.header}>Your Location on the Map</h1>
+      <MapContainer center={[lat, lng]} zoom={18} className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
